4df5ad079b88dd15c98225676bbc2339e048b4be
zhouyuqin <zhouyuqin@zhouyuqin.cn>
2015-05-31 14:28:25 +0800

    完成

--- src/main/java/MergeData.java
+++ src/main/java/MergeData.java
@@ -1,16 +1,94 @@
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Created by azure on 2015/5/30 0030.
  */
 public class MergeData
 {
+    private static Logger logger = LogManager.getLogger(MergeData.class);
+
+    private static Path getPath(String basePath, String fileName)
+    {
+        System.out.print("输入" + fileName + "文件名(或全路径): ");
+        Scanner scanner = new Scanner(System.in);
+        String p = scanner.next();
+        Path path1 = null;
+        try
+        {
+            while (true)
+            {
+                if (p.indexOf(":") == -1)
+                {
+                    path1 = Paths.get(basePath + p);
+                } else
+                {
+                    path1 = Paths.get(p);
+                }
+                if (!path1.toFile().exists())
+                {
+                    System.out.print("文件不存在，请重新输入" + fileName + "文件名(或全路径): ");
+                    p = scanner.next();
+                } else
+                {
+                    logger.info("文件存在，文件路径：" + path1.toRealPath());
+                    break;
+                }
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return path1;
+    }
+
+    private static String getInput(String prom)
+    {
+        System.out.print(prom + ": ");
+        Scanner scanner = new Scanner(System.in);
+        String p = scanner.next();
+        return p;
+    }
+
+    private static String getInput(String[] title, String prom)
+    {
+        prom = "请从" + Arrays.toString(title) + "中选择 " + prom + " : ";
+        System.out.print(prom);
+        Scanner scanner = new Scanner(System.in);
+        String p = scanner.next();
+        while (true)
+        {
+            int i = 0;
+            for (; i < title.length; i++)
+            {
+                if (p.equals(title[i]))
+                {
+                    break;
+                }
+            }
+            if (i == title.length)
+            {
+                System.out.print("不存在该值，" + prom.replace("请", "请重新"));
+                p = scanner.next();
+            } else if (i < title.length)
+            {
+                break;
+            }
+        }
+        return p;
+    }
+
+
     /**
      * args[0]:Excel_1 path
      * args[1]:Excel_2 path
@@ -22,43 +100,26 @@ public class MergeData
      */
     public static void main(String[] args) throws Exception
     {
-        Path path1 = null;
-        Path path2 = null;
-        String path =  MergeData.class.getProtectionDomain().getCodeSource().getLocation().toString();
-        if(path.endsWith(".jar"))
-        {
-            path = path.substring(0,path.lastIndexOf("/")+1);
-        }
-        System.out.println(path);
-        path = path.replace("file:/", "");
-        if (args[0].indexOf(":") == -1)
-        {
-            path1 = Paths.get(path + args[0]);
-        } else
-        {
-            path1 = Paths.get(args[0]);
-        }
-        if (args[1].indexOf(":") == -1)
-        {
-            path2 = Paths.get(path + args[1]);
-        } else
-        {
-            path2 = Paths.get(args[1]);
-        }
 
+        String path = MergeData.class.getProtectionDomain().getCodeSource().getLocation().toString();
+        if (path.endsWith(".jar"))
+        {
+            path = path.substring(0, path.lastIndexOf("/") + 1);
+        }
+        path = path.replace("file:/", "");
+        Path path1 = getPath(path, "第一个");
+        Path path2 = getPath(path, "第二个");
         HandleData handleData = new HandleData();
-        handleData.column1 = args[3];
         handleData.path = path1;
         handleData.resultPathStr = path1.toRealPath().toString().replace(".xls", "_result.xls");
-        HandleData.logger.info(path1 + "," + path1.toFile().exists() + "," + handleData.column1 + "," + handleData.column2);
         InputStream inputStream = new FileInputStream(path1.toRealPath().toString());
         String[] title1 = handleData.title = handleData.readExcelTitle(inputStream);
         handleData.data1 = new ArrayList<>();
         handleData.readExcelContent(handleData.data1);
-
         handleData.path = path2;
         inputStream = new FileInputStream(path2.toRealPath().toString());
         String[] title2 = handleData.title = handleData.readExcelTitle(inputStream);
+        handleData.column1 = getInput(title2, "合并列名");
         handleData.data2 = new ArrayList<>();
         handleData.readExcelContent(handleData.data2);
         handleData.updateUID(handleData.data1, "year", "id");
@@ -68,11 +129,18 @@ public class MergeData
         handleData.mergeData(handleData.resultData, handleData.data2, handleData.column1);
         String[] title = new String[title1.length + 1];
         title = Arrays.copyOf(title1, title.length);
-        title[title.length-1] = handleData.column1;
+        title[title.length - 1] = handleData.column1;
         handleData.title = title;
-        HandleData.logger.info("title:"+Arrays.toString(title));
-        handleData.data2 = new ArrayList<>();
-        handleData.sortData(handleData.resultData, handleData.data2 ,handleData.title[0]);
+        HandleData.logger.info("title:" + Arrays.toString(title));
+        handleData.data2 = null;
+        if (getInput("是否需要排序(yes/no)").toLowerCase().indexOf("y") != -1)
+        {
+            handleData.data2 = new ArrayList<>();
+            String sortName = getInput(title, "排序名");
+
+            handleData.sortData(handleData.resultData, handleData.data2, sortName);
+        }
         handleData.writeResultData(handleData.data2);
     }
+
 }
